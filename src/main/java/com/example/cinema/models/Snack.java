f503c2347320e67dba82c0a1561c4320e49fe5b9
IT25100604 <it25100604@my.sliit.lk>
2026-05-05 14:25:57 +0530

    Add Snack model class with attributes Add default and parameterized constructors to Snack Add getters and setters to Snack class Add toString method to Snack class Update Snack prices to Sri Lankan Rupees

--- src/main/java/com/example/cinema/models/Snack.java
+++ src/main/java/com/example/cinema/models/Snack.java
@@ -1,4 +1,77 @@
 package com.example.cinema.models;
 
 public class Snack {
+
+    private String snackId;
+    private String name;
+    private double price;
+    private String category;
+    private String imagePath;
+
+    public Snack() {
+        this.snackId   = "";
+        this.name      = "";
+        this.price     = 0.0;
+        this.category  = "";
+        this.imagePath = "";
+    }
+
+    public Snack(String snackId, String name, double price,
+                 String category, String imagePath) {
+        this.snackId   = snackId;
+        this.name      = name;
+        this.price     = price;
+        this.category  = category;
+        this.imagePath = imagePath;
+    }
+
+    public String getSnackId() {
+        return snackId;
+    }
+
+    public void setSnackId(String snackId) {
+        this.snackId = snackId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String toString() {
+        return "Snack{" +
+                "snackId='"    + snackId   + '\'' +
+                ", name='"     + name      + '\'' +
+                ", price="     + price     +
+                ", category='" + category  + '\'' +
+                ", imagePath='"+ imagePath + '\'' +
+                '}';
+    }
 }
