7e64a5313cc1468c6b8534af42acd5b59c9af5b6
IT25100604 <it25100604@my.sliit.lk>
2026-05-13 23:08:39 +0530

    Add Snack model with private attributes

--- src/main/java/com/example/cinema/models/Snack.java
+++ src/main/java/com/example/cinema/models/Snack.java
@@ -1,13 +1,16 @@
 package com.example.cinema.models;
 
+// Snack model class represents a snack item in the cinema
 public class Snack {
 
+    // Private attributes for snack details
     private String snackId;
     private String name;
     private double price;
     private String category;
     private String imagePath;
 
+    // Default constructor - initializes snack with empty values
     public Snack() {
         this.snackId   = "";
         this.name      = "";
@@ -16,6 +19,7 @@ public class Snack {
         this.imagePath = "";
     }
 
+    // Parameterized constructor - initializes snack with given values
     public Snack(String snackId, String name, double price,
                  String category, String imagePath) {
         this.snackId   = snackId;
@@ -25,46 +29,57 @@ public class Snack {
         this.imagePath = imagePath;
     }
 
+    // Getter for snack ID
     public String getSnackId() {
         return snackId;
     }
 
+    // Setter for snack ID
     public void setSnackId(String snackId) {
         this.snackId = snackId;
     }
 
+    // Getter for snack name
     public String getName() {
         return name;
     }
 
+    // Setter for snack name
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter for snack price
     public double getPrice() {
         return price;
     }
 
+    // Setter for snack price
     public void setPrice(double price) {
         this.price = price;
     }
 
+    // Getter for snack category
     public String getCategory() {
         return category;
     }
 
+    // Setter for snack category
     public void setCategory(String category) {
         this.category = category;
     }
 
+    // Getter for snack image path
     public String getImagePath() {
         return imagePath;
     }
 
+    // Setter for snack image path
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
 
+    // Returns a string representation of the snack object
     public String toString() {
         return "Snack{" +
                 "snackId='"    + snackId   + '\'' +
