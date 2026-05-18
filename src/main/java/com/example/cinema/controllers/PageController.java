028558a6ef3f3a7c247b334d717fbf96c807904b
IT25102129 <it25102129@my.sliit.lk>
2026-05-18 23:16:10 +0530

    data base storing the movie and show the home page movie grid for

--- src/main/java/com/example/cinema/controllers/PageController.java
+++ src/main/java/com/example/cinema/controllers/PageController.java
@@ -1,20 +1,27 @@
 package com.example.cinema.controllers;
 
-
+import com.example.cinema.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+    @Autowired
+    private MovieRepository movieRepository;
+
+    // data base store movies send index.html
     @GetMapping("/")
-    public String showHomaPage(){
-        return "index";  // show then index.html
+    public String showHomePage(Model model){
+        model.addAttribute("movies",movieRepository.findAll());
+        return "index";
     }
-    @GetMapping("/details")
-    public String showMovieDetails(){
-        return "Movie-details"; // show movie details page
-    }
+//    @GetMapping("/details")
+//    public String showMovieDetails(){
+//        return "Movie-details"; // show movie details page
+//    }
     @GetMapping("/add-movie-form")
     public String showAddMovieForm(){
         return "add-movie-form"; //show movie form page
