525ad5176fe5c9e54e992d3912f2f5fc1b8632a6
IT25102129 <it25102129@my.sliit.lk>
2026-05-18 14:50:29 +0530

    Movie controller class update to for data base using .annotation and movie repository connected. viwe all movies and delete the movie (getmapping and postmapping use ui conected with)

--- src/main/java/com/example/cinema/controllers/MovieController.java
+++ src/main/java/com/example/cinema/controllers/MovieController.java
@@ -1,19 +1,20 @@
 package com.example.cinema.controllers;
 
 import com.example.cinema.models.Movie;
-import com.example.cinema.services.MovieManager;
+import com.example.cinema.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller // This tells Spring Boot that this class handles web traffic
+@RequestMapping("/movies")
 public class MovieController {
 
-    // Instantiate our manager to handle the file writing
-    private final MovieManager movieManager = new MovieManager();
-
+    @Autowired
+    private MovieRepository movieRepository;
     // ==========================================
     // 1. SHOW THE HTML FORM
     // ==========================================
@@ -34,13 +35,25 @@ public class MovieController {
     @PostMapping("/movies/add")
     public String saveMovie(@ModelAttribute("movie") Movie movie) {
 
-        // Let's auto-generate a unique ID for the movie based on the current time
-        movie.setMovieId("M-" + System.currentTimeMillis());
+        if (movie.getMovieId() == null || movie.getMovieId().isEmpty()) {
+            movie.setMovieId("MOV-" + UUID.randomUUID().toString().substring(0, 5).toUpperCase());
+        }
 
-        // Save the movie to "movies.txt" using the manager we built earlier
-        movieManager.addMovie(movie);
+        // Save the movie to data base
+        movieRepository.save(movie);
 
         // Redirect the user back to the form (you can change this to a success page later)
         return "redirect:/movies/add?success";
+    }
+    @GetMapping("/movies/all")
+    public String viweAllMovies(Model model){
+        model.addAttribute("movies",movieRepository.findAll());
+
+        return "movie-list";
+    }
+    @PostMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable String id) {
+        movieRepository.deleteById(id);
+        return "redirect:/movies/all";
     }
 }
