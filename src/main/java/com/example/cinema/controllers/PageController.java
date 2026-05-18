7c258d755bfab1d032bb7b515d076a761ad29167
IT25102129 <it25102129@my.sliit.lk>
2026-05-18 19:00:31 +0530

    get mapping

--- src/main/java/com/example/cinema/controllers/PageController.java
+++ src/main/java/com/example/cinema/controllers/PageController.java
@@ -11,11 +11,11 @@ public class PageController {
     public String showHomaPage(){
         return "index";  // show then index.html
     }
-
+    @GetMapping("/details")
     public String showMovieDetails(){
         return "Movie-details"; // show movie details page
     }
-
+    @GetMapping("/add-movie-form")
     public String showAddMovieForm(){
         return "add-movie-form"; //show movie form page
     }
