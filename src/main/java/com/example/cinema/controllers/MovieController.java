2fad55b56481c89c47746f0556fbcda1956b886a
IT25102129 <it25102129@my.sliit.lk>
2026-05-20 00:27:49 +0530

    Error fix - Add new movie and create the uniq movie id.

--- src/main/java/com/example/cinema/controllers/MovieController.java
+++ src/main/java/com/example/cinema/controllers/MovieController.java
@@ -32,48 +32,63 @@ public class MovieController {
     @PostMapping("/add")
     public String saveMovie(@ModelAttribute("movie") Movie movie) {
 
-        if (movie.getMovieId() == null || movie.getMovieId().isEmpty()) {
-            movie.setMovieId("MOV-" + UUID.randomUUID().toString().substring(0, 5).toUpperCase());
-        }
+        //Add the new movie and create uniq movie id
+        String uniqueID = UUID.randomUUID().toString();
+        movie.setMovieId("MOV-" + uniqueID.substring(0, 8).toUpperCase());
 
-        // Save the movie to data base
+        // Save the movie to database
         movieRepository.save(movie);
 
         // Redirect the user back to the form (you can change this to a success page later)
         return "redirect:/movies/add?success";
     }
+
+    //View all movies for database (Admin dashboard)
     @GetMapping("/all")
     public String viweAllMovies(Model model){
+
         model.addAttribute("movies",movieRepository.findAll());
 
         return "admin-dashboard";
     }
+
+    //  Movies delete
     @PostMapping("/delete/{id}")
     public String deleteMovie(@PathVariable String id) {
+
         movieRepository.deleteById(id);
         return "redirect:/movies/all";
     }
+
+    //view movie Details
     @GetMapping("/view/{id}")
     public String viewMovie(@PathVariable String id, Model model) {
+
         model.addAttribute("movie", movieRepository.findById(id).orElseThrow());
         return "movie-details";
     }
 
+    // Display the form for updating Movie
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable String id, Model model) {
+
         model.addAttribute("movie", movieRepository.findById(id).orElseThrow());
         return "update-movie-form";
     }
 
+    //Update movies database save!
     @PostMapping("/update/{id}")
     public String updateMovie(@PathVariable String id, @ModelAttribute Movie movie) {
-        movie.setMovieId(id);
-        movieRepository.save(movie);
-        return "redirect:/movies/dashboard";
+
+        movie.setMovieId(id);  //old movie id
+        movieRepository.save(movie); //Update the new data
+        return "redirect:/movies/all";
     }
 
+    // show admin dashboard
     @GetMapping("/dashboard")
     public String showDashboard(Model model) {
+
         model.addAttribute("movies", movieRepository.findAll());
         return "admin-dashboard";
     }
